47c399f7e80c3762fd5420f4370c9722ddf4f940
Nikil456 <kandala.nikil@gmail.com>
2024-02-23 17:47:59 -0500

    Created Input Meal Screen.

---
+++ app/src/main/java/com/example/cs2340a_team1/views/inputMealActivity.java
@@ -0,0 +1,18 @@
+package com.example.cs2340a_team1.views;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cs2340a_team1.R;
+
+
+public class inputMealActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inputmealscreen);
+    }
+}
