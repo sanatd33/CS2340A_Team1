8c3cd12cf4bcb91f3314ad893752cc0fc14f61bf
sramesh3 <sanjayguru2005@gmail.com>
2024-02-23 19:03:12 -0500

    added import

--- app/src/main/java/com/example/cs2340a_team1/views/HomeActivity.java
+++ app/src/main/java/com/example/cs2340a_team1/views/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import com.example.cs2340a_team1.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
