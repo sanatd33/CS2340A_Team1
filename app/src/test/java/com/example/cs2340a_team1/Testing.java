380d1f81f330d914c2baa525dd543e3f923c31bc
smoussa3 <smoussa3@gatech.edu>
2024-03-12 16:25:22 -0400

    add j-unit to check if user is valid (no null, whitespace, and empty input). Also added the functionality to related clasess

--- app/src/test/java/com/example/cs2340a_team1/Testing.java
+++ app/src/test/java/com/example/cs2340a_team1/Testing.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs2340a_team1.model.UserData;
-import com.example.cs2340a_team1.viewmodels.LoginViewModel;
 import com.example.cs2340a_team1.viewmodels.UserViewModel;
 import com.example.cs2340a_team1.views.LoginActivity;
 import com.google.firebase.database.DatabaseReference;
@@ -22,43 +21,37 @@ import com.google.firebase.database.FirebaseDatabase;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class Testing {
-    @Test
-    public void nullChecker() { //detect null inputs
-        UserViewModel user = new UserViewModel();
-        user.updateUser(null);
-        user.updatePass(null);
-        UserData data = user.getUserData();
-        String username = data.getUser();
-        String password = data.getPass();
+    @Test public void checkValidUser() {
+        //when user is valid
+        UserViewModel user1 = new UserViewModel();
+        user1.updateUser("Testing");
+        user1.updatePass("1234");
+        UserData data1 = user1.getUserData();
+        boolean usernameValid1 = data1.isUsernameValid(data1.getUser());
+        boolean passwordValid1 = data1.isPasswordValid(data1.getPass());
+        //expected to be valid (true)
+        assertTrue(usernameValid1);
+        assertTrue(passwordValid1);
 
-        assertNull(password);
-        assertNull(username);
-    }
-    @Test
-    public void spaceChecker() { //detect space inputs
-        boolean condition = false; // True if detect empty space for both password and username
-        UserViewModel user = new UserViewModel();
-        user.updateUser("Sami Moussa");
-        user.updatePass("12 34");
-        UserData data = user.getUserData();
-        String username = data.getUser();
-        String password = data.getPass();
+        //when user is not valid
+        UserViewModel user2 = new UserViewModel();
+        user2.updateUser("White Space"); //there is a space in username
+        user2.updatePass(null); //password is set to null
+        UserData data2 = user2.getUserData();
+        boolean usernameValid2 = data2.isUsernameValid(data2.getUser());
+        boolean passwordValid2 = data2.isPasswordValid(data2.getPass());
+        //expected to not be valid (false)
+        assertFalse(usernameValid2);
+        assertFalse(passwordValid2);
 
-        if (username.contains(" ") && password.contains(" ")) {
-            condition = true;
-        }
-        assertTrue(condition);
-    }
-    @Test
-    public void emptyChecker() {//detect empty inputs
-        UserViewModel user = new UserViewModel();
-        user.updateUser("");
-        user.updatePass("");
-        UserData data = user.getUserData();
-        String username = data.getUser();
-        String password = data.getPass();
-        assertEquals("", username);
-        assertEquals("", password);
+        //Lastly when user doesn't input anything
+        UserViewModel user3 = new UserViewModel();
+        UserData data3 = user3.getUserData();
+        boolean usernameValid3 = data3.isUsernameValid(data3.getUser());
+        boolean passwordValid3 = data3.isPasswordValid(data3.getPass());
+        //expected to not be valid (false)
+        assertFalse(usernameValid3);
+        assertFalse(passwordValid3);
     }
 
     @Test
