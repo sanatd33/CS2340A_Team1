675b53bdc95cf39c9f94a3f355eada7333375886
sanatd33 <saisanat@gmail.com>
2024-04-21 23:51:01 -0400

    asdf

--- app/src/main/java/com/example/cs2340a_team1/views/ShoppingList.java
+++ app/src/main/java/com/example/cs2340a_team1/views/ShoppingList.java
@@ -2,7 +2,6 @@ package com.example.cs2340a_team1.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.core.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
